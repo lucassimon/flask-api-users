83d88afd1c4d2cf8b8303cd81e9fcb43c0e9e3da
Lucas Simon <lucassrod@gmail.com>
2018-06-12 20:04:31 -0300

    Setup configure api

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,13 +1,18 @@
 # -*- coding: utf-8 -*-
 
-
 from flask import Flask
 from config import config
+
+# Realize a importação da função que configura a api
+from .api import configure_api
 
 
 def create_app(config_name):
     app = Flask('api-users')
 
     app.config.from_object(config[config_name])
+
+    # executa a chamada da função de configuração
+    configure_api(app)
 
     return app
