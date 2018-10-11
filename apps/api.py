a898d3aba5a0f712835c254db05c0f02c5c4e487
Lucas Simon <lucassrod@gmail.com>
2018-10-11 16:40:17 -0300

    Crate a new endpoint to fetch users

--- apps/api.py
+++ apps/api.py
@@ -5,6 +5,7 @@
 from flask_restful import Api, Resource
 
 from apps.users.resources import SignUp
+from apps.users.resources_admin import AdminUserPageList
 
 
 # Criamos uma classe que extende de Resource
@@ -24,11 +25,13 @@ api = Api()
 
 def configure_api(app):
 
-    # adicionamos na rota '/' a sua classe correspondente Index
     api.add_resource(Index, '/')
 
     # rotas para o endpoint de usuarios
     api.add_resource(SignUp, '/users')
+
+    # rotas para os admins
+    api.add_resource(AdminUserPageList, '/admin/users/<int:page_id>')
 
     # inicializamos a api com as configurações do flask vinda por parâmetro
     api.init_app(app)
