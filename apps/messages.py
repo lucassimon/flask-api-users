0a5ae0ebc416a0ae60ac75a689e06d71fa6dfe4e
Lucas Simon <lucassrod@gmail.com>
2018-10-11 00:29:21 -0300

    Create a messages to standarlizes the return messages

---
+++ apps/messages.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+MSG_FIELD_REQUIRED = 'Campo obrigatório.'
+MSG_INVALID_DATA = 'Ocorreu um erro nos campos informados.'
+MSG_DOES_NOT_EXIST = 'Este(a) {} não existe.'
+MSG_EXCEPTION = 'Ocorreu um erro no servidor. Contate o administrador.'
+MSG_ALREADY_EXISTS = 'Já existe um(a) {} com estes dados.'
+MSG_NO_DATA = 'Nenhum dado foi postado.'
+MSG_PASSWORD_WRONG = 'As senhas não conferem.'
+
+MSG_RESOURCE_CREATED = '{} criado(a).'
