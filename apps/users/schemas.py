105284640ff1ff273aa98dc3011308fb140a4440
Lucas Simon <lucassrod@gmail.com>
2018-10-11 00:30:17 -0300

    Create schemas to input and output data

---
+++ apps/users/schemas.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+
+from marshmallow import Schema
+from marshmallow.fields import Email, Str, Boolean
+
+from apps.messages import MSG_FIELD_REQUIRED
+
+
+class UserRegistrationSchema(Schema):
+    full_name = Str(
+        required=True, error_messages={'required': MSG_FIELD_REQUIRED}
+    )
+    email = Email(
+        required=True, error_messages={'required': MSG_FIELD_REQUIRED}
+    )
+    password = Str(
+        required=True, error_messages={'required': MSG_FIELD_REQUIRED}
+    )
+
+
+class UserSchema(Schema):
+    full_name = Str(
+        required=True, error_messages={'required': MSG_FIELD_REQUIRED}
+    )
+    email = Email(
+        required=True, error_messages={'required': MSG_FIELD_REQUIRED}
+    )
+    cpf_cnpj = Str()
+    active = Boolean()
