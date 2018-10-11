cd98108d6722462de54b3c9b79d6d336768ae5c2
Lucas Simon <lucassrod@gmail.com>
2018-10-11 00:28:38 -0300

    Adds a new endpoint

--- apps/api.py
+++ apps/api.py
@@ -4,6 +4,8 @@
 # Importamos as classes API e Resource
 from flask_restful import Api, Resource
 
+from apps.users.resources import SignUp
+
 
 # Criamos uma classe que extende de Resource
 class Index(Resource):
@@ -24,6 +26,9 @@ def configure_api(app):
 
     # adicionamos na rota '/' a sua classe correspondente Index
     api.add_resource(Index, '/')
+
+    # rotas para o endpoint de usuarios
+    api.add_resource(SignUp, '/users')
 
     # inicializamos a api com as configurações do flask vinda por parâmetro
     api.init_app(app)
