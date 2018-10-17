37d1c4b6425fd284c1a1046051d6f291c7020637
Lucas Simon <lucassrod@gmail.com>
2018-10-17 15:02:40 -0300

    Implements PUT in AdminUserResource

--- apps/users/resources_admin.py
+++ apps/users/resources_admin.py
@@ -6,14 +6,19 @@ from flask import request
 # Third
 from flask_restful import Resource
 from mongoengine.errors import FieldDoesNotExist
+from mongoengine.errors import NotUniqueError, ValidationError
 
 # Apps
-from apps.responses import resp_ok, resp_exception
+from apps.responses import resp_ok, resp_exception, resp_data_invalid, resp_already_exists
+
 from apps.messages import MSG_RESOURCE_FETCHED_PAGINATED, MSG_RESOURCE_FETCHED
+from apps.messages import MSG_NO_DATA, MSG_RESOURCE_UPDATED, MSG_INVALID_DATA
+from apps.messages import MSG_ALREADY_EXISTS
 
 # Local
 from .models import User
-from .schemas import UserSchema
+from .schemas import UserSchema, UserUpdateSchema
+from .utils import get_user_by_id, exists_email_in_users
 
 
 class AdminUserPageList(Resource):
@@ -64,12 +69,65 @@ class AdminUserResource(Resource):
         result = None
         schema = UserSchema()
 
-        try:
-            # buscamos todos os usuarios da base utilizando o paginate
-            user = User.objects.get(id=user_id)
+        user = get_user_by_id(user_id)
 
-        except FieldDoesNotExist as e:
-            return resp_exception('Users', description=e.__str__())
+        if not isinstance(user, User):
+            return user
+
+        result = schema.dump(user)
+
+        return resp_ok(
+            'Users', MSG_RESOURCE_FETCHED.format('Usuários'),  data=result.data
+        )
+
+    def put(self, user_id):
+        result = None
+        schema = UserSchema()
+        update_schema = UserUpdateSchema()
+        req_data = request.get_json() or None
+        email = None
+
+        # Valido se o payload está vazio
+        if req_data is None:
+            return resp_data_invalid('Users', [], msg=MSG_NO_DATA)
+
+        # Busco o usuário na coleção users pelo seu id
+        user = get_user_by_id(user_id)
+
+        # se não for uma instancia do modelo User retorno uma resposta
+        # da requisição http do flask
+        if not isinstance(user, User):
+            return user
+
+        # carrego meus dados de acordo com o schema de atualização
+        data, errors = update_schema.load(req_data)
+
+        # em caso de erros retorno uma resposta 422 com os erros de
+        # validação do schema
+        if errors:
+            return resp_data_invalid('Users', errors)
+
+        email = data.get('email', None)
+
+        # Valido se existe um email na coleção de usuários
+        if email and exists_email_in_users(email, user):
+            return resp_data_invalid(
+                'Users', [{'email': [MSG_ALREADY_EXISTS.format('usuário')]}]
+            )
+
+        try:
+            # para cada chave dentro do dados do update schema
+            # atribuimos seu valor
+            for i in data.keys():
+                user[i] = data[i]
+
+            user.save()
+
+        except NotUniqueError:
+            return resp_already_exists('Users', 'usuário')
+
+        except ValidationError as e:
+            return resp_exception('Users', msg=MSG_INVALID_DATA, description=e.__str__())
 
         except Exception as e:
             return resp_exception('Users', description=e.__str__())
@@ -77,5 +135,5 @@ class AdminUserResource(Resource):
         result = schema.dump(user)
 
         return resp_ok(
-            'Users', MSG_RESOURCE_FETCHED.format('Usuários'),  data=result.data
+            'Users', MSG_RESOURCE_UPDATED.format('Usuário'),  data=result.data
         )
