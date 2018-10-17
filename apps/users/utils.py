ab7a10dde46e11c7921c3a10fdb02f5eb983b5a0
Lucas Simon <lucassrod@gmail.com>
2018-10-17 15:02:17 -0300

    Implements get_user_by_id and exists_email_in_users

--- apps/users/utils.py
+++ apps/users/utils.py
@@ -1,7 +1,17 @@
 # -*- coding: utf-8
 
+# Third
 
-def check_password_in_signup(password, confirm_password):
+from mongoengine.errors import FieldDoesNotExist, DoesNotExist, MultipleObjectsReturned
+
+# Apps
+from apps.responses import resp_exception, resp_does_not_exist
+
+# Local
+from .models import User
+
+
+def check_password_in_signup(password: str, confirm_password: str):
 
     if not password:
         return False
@@ -10,6 +20,44 @@ def check_password_in_signup(password, confirm_password):
         return False
 
     if not password == confirm_password:
+        return False
+
+    return True
+
+
+def get_user_by_id(user_id: str):
+    try:
+        # buscamos todos os usuários da base utilizando o paginate
+        return User.objects.get(id=user_id)
+
+    except DoesNotExist as e:
+        return resp_does_not_exist('Users', 'Usuário')
+
+    except FieldDoesNotExist as e:
+        return resp_exception('Users', description=e.__str__())
+
+    except Exception as e:
+        return resp_exception('Users', description=e.__str__())
+
+
+def exists_email_in_users(email: str, instance=None):
+    """
+    Verifico se existe um usuário com aquele email
+    """
+    user = None
+
+    try:
+        user = User.objects.get(email=email)
+
+    except DoesNotExist:
+        return False
+
+    except MultipleObjectsReturned:
+        return True
+
+    # verifico se o id retornado na pesquisa é mesmo da minha instancia
+    # informado no parâmetro
+    if instance and instance.id == user.id:
         return False
 
     return True
