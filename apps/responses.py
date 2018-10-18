7f4ee1f0f30b634e03c62f353b1d6887b0df0474
Lucas Simon <lucassrod@gmail.com>
2018-10-17 21:12:10 -0300

    Implements the method to responses not allowed

--- apps/responses.py
+++ apps/responses.py
@@ -3,7 +3,7 @@
 from flask import jsonify
 
 from .messages import MSG_INVALID_DATA, MSG_DOES_NOT_EXIST, MSG_EXCEPTION
-from .messages import MSG_ALREADY_EXISTS
+from .messages import MSG_ALREADY_EXISTS, MSG_PERMISSION_DENIED
 
 
 def resp_data_invalid(
@@ -99,5 +99,20 @@ def resp_ok(resource: str, message: str, data=None, **extras):
     resp = jsonify(response)
 
     resp.status_code = 200
+
+    return resp
+
+
+def resp_notallowed_user(resource: str, msg: str = MSG_PERMISSION_DENIED):
+    if not isinstance(resource, str):
+        raise ValueError('Recurso precisa ser uma string')
+
+    resp = jsonify({
+        'status': 401,
+        'resource': resource,
+        'message': msg
+    })
+
+    resp.status_code = 401
 
     return resp
