40ae583f0f342b91bb173329aadc9b20eb071e38
Lucas Simon <lucassrod@gmail.com>
2018-10-17 21:10:47 -0300

    Adds new configs

--- config.py
+++ config.py
@@ -2,6 +2,7 @@
 
 # Python
 from os import getenv
+from datetime import timedelta
 
 
 class Config:
@@ -9,6 +10,12 @@ class Config:
     APP_PORT = int(getenv('APP_PORT'))
     DEBUG = eval(getenv('DEBUG').title())
     MONGODB_HOST = getenv('MONGODB_URI')
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(
+        minutes=int(getenv('JWT_ACCESS_TOKEN_EXPIRES'))
+    )
+    JWT_REFRESH_TOKEN_EXPIRES = timedelta(
+        days=int(getenv('JWT_REFRESH_TOKEN_EXPIRES'))
+    )
 
 
 class DevelopmentConfig(Config):
