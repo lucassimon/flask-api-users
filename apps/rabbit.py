c5ea08a3fd1d92c8894829cacd7c8baf020c5eed
Lucas Simon <lucassrod@gmail.com>
2018-12-07 19:57:11 -0200

    Implements a RabbitMQ class with pika conn

--- apps/rabbit.py
+++ apps/rabbit.py
@@ -2,14 +2,24 @@
 
 # Third
 from pika import BlockingConnection, URLParameters
+from flask import current_app
 
 
-def connection_rabbit(uri):
+class RabbitMQ:
 
-    if uri:
-        params = URLParameters(uri)
-        params.socket_timeout = 5
-
-        return BlockingConnection(params)
-
-    return None
+    @staticmethod
+    def connect():
+        params = URLParameters(current_app.config.get('AMQP_URI'))
+        # number of socket connection attempts
+        params.connection_attempts = 7
+        # interval between socket connection attempts; see also connection_attempts.
+        params.retry_delay = 300
+        # AMQP connection heartbeat timeout value for negotiation during connection
+        # tuning or callable which is invoked during connection tuning
+        params.heartbeat = 600
+        # None or float blocked connection timeout
+        params.blocked_connection_timeout = 300
+        try:
+            return BlockingConnection(params)
+        except Exception as e:
+            raise
